58748c3d9dd0a9e133292c04ad4eb98a9e0717f5
Pavel <pavelyeganov@gmail.com>
2019-03-14 17:27:42 -0400

    fixed

--- config/prod.js
+++ config/prod.js
@@ -8,5 +8,5 @@ module.exports = {
   twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
   twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
   twitterAccessToken: process.env.TWITTER_ACCESS_TOKEN,
-  twitterAccessSecretToken: process.env.TWITTER_ACCESS_SECRET_TOKEN
+  twitterAccessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
