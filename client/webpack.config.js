82f6e4456c1c24a6494ce51e23cf144dc68feb6c
Pavel <pavelyeganov@gmail.com>
2019-03-14 17:48:19 -0400

    oof

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: __dirname + "/build",
     filename: "bundle.js",
-    publicPath: '/build'
+    publicPath: '/'
   },
   //devtool: 'source-map',
   module: {
