b1e54aca2b8e6300f6e41bc8af6b2f31386ad584
pagnito <pagnito@gmail.com>
2020-03-21 22:24:58 -0400

    wtffffff

--- database/redis.js
+++ database/redis.js
@@ -1,4 +1,4 @@
 const redis = require('redis');
-let url = process.env.NODE_ENV === 'production' ? process.env.REDISCLOUD_URL : 'redis://127.0.0.1:6379';
+let url = process.env.NODE_ENV === 'production' ? process.env.REDIS_URL : 'redis://127.0.0.1:6379';
 let redClient = redis.createClient(url);
 module.exports = redClient;
