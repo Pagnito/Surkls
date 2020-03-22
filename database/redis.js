3110f016b6e470f737d88994d6d3aa5866b8c4aa
pagnito <pagnito@gmail.com>
2020-03-21 22:18:49 -0400

    yo fck redis labs heroky service, where the fuck is the uri

--- database/redis.js
+++ database/redis.js
@@ -1,8 +1,4 @@
 const redis = require('redis');
 let url = process.env.NODE_ENV === 'production' ? process.env.REDISCLOUD_URL : 'redis://127.0.0.1:6379';
-let redClient = redis.createClient({
-  host: url,
-  no_ready_check: true,
-  auth_pass: process.env.REDIS_PASSWORD,
-});
+let redClient = redis.createClient(url);
 module.exports = redClient;
